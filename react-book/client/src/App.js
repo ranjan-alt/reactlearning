79eb6fecd6259180c78dadfb48655dedd2acee85
Ranjan Kumar <67799181+ranjan-alt@users.noreply.github.com>
2023-06-10 22:28:37 +0000

    components created

--- react-book/client/src/App.js
+++ react-book/client/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 // import ProductList from './components/project1/ProductList/ProductList';
+import TimersDashboard from './components/Project2/TimersDashboard';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         {/* <ProductList/> */}
+        <TimersDashboard />
       </header>
     </div>
   );
